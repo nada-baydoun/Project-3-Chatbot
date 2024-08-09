03177303a0d283646f252a122860a259cad8e238
MhaamadAli <m.alialaawar@gmail.com>
2024-08-09 20:01:03 +0300

    modified ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
+  apps: [
+    {
+      script: 'npm start',
+    },
+  ],
 
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
+  deploy: {
+    production: {
+      key: 'ec2-chat-bot.pem',
+      user: 'ubuntu',
+      host: '54.198.105.1',
+      ref: 'origin/main',
+      repo: 'git@github.com/MhaamadAli/chatbot.git',
+      path: '/home/ubuntu',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+      'post-deploy': 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      ssh_options: 'ForwardAgent=yes',
+    },
+  },
 };
